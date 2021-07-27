6ce131dde90d68c55fa965b76c2c8ddf5a9232a4
Naveen <catchspider2002@gmail.com>
2021-07-28 02:16:07 +0530

    Fixed the quotes in constants svg parameter which was causing the issue

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -1,8 +1,5 @@
 import { writable } from 'svelte/store';
-// import { onMount } from "svelte";
 
 export const themeStore = writable("light");
 export const langStore = writable("pl");
 export const webStore = writable("https://pl.pattern.monster");
-
-// export let themeStore;
